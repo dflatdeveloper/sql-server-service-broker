6966f251af1a3dbecd730044bab68166522922d6
David Bailey <github-mycareer@dflatdeveloper.net>
2024-11-01 00:02:03 -0400

    Added Route Generation Bypass for first Run

--- ServiceBrokerA/Script.PostDeployment1.sql
+++ ServiceBrokerA/Script.PostDeployment1.sql
@@ -1,5 +1,15 @@
-﻿:r ".\Objects\Routes\Route_In.sql"
+﻿IF CAST('$(GENERATE_ROUTES)' AS BIT) = 0
+BEGIN
+	PRINT 'SKIPPING ROUTE GENERATION'
+
+	GOTO ROUTE_CREATION_COMPLETE
+END
+
+:r ".\Objects\Routes\Route_In.sql"
 :r ".\Objects\Routes\Route_Out.sql"
+
+ROUTE_CREATION_COMPLETE:
+
 :r ".\Objects\Remote Service Binding\Create Inbound Binding.sql"
 :r ".\Objects\Remote Service Binding\Create Outbound Binding.sql"
 :r ".\user\security\Grant Connect To Service.sql"
